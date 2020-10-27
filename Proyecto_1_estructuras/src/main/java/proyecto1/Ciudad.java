e72af2872009df9a45104d4af66888e0ff98cdc7
RicardoMicale <71532222+RicardoMicale@users.noreply.github.com>
2020-10-27 11:40:41 -0400

    Add files via upload

---
+++ Proyecto_1_estructuras/src/main/java/proyecto1/Ciudad.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author ricar
+ */
+public class Ciudad {
+    int id;
+    String nombre;
+
+    public Ciudad(int id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
